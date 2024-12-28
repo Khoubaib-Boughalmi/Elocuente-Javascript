cfe8ee3e7f10e12bce9d339bc2cea60a5458d3d8
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2024-12-28 19:27:54 +0100

    optimization in js runtime

---
+++ extra/optimization.js
@@ -0,0 +1,11 @@
+/**
+  . Optimizations During Runtime
+
+    Several optimizations can happen during runtime:
+
+    * JIT (Just-In-Time) Compilation: In JavaScript, the engine (v8 for chrome) often uses JIT compilation to convert code into machine code just before execution, improving performance.
+    
+    * Inlining and Caching: The runtime may optimize repeated function calls by inlining small functions or caching results for reuse.
+
+    * Lazy Loading: In some runtime environments, parts of the code or libraries may be loaded only when needed, improving startup time.
+ */
